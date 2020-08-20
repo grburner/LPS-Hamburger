416b18a716550c8355e4fb0dbcd4882692a4f0a4
Gary Burner <grburner@gmail.com>
2020-08-19 21:50:14 -0400

    fixed schema.sql error

--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,12 @@
+DROP DATABASE IF EXISTS burgers_db;
+
 CREATE DATABASE burgers_db;
 
 USE burgers_db;
 
 CREATE TABLE burgers (
-    id INT AUTO INCREMENT,
+    id INT AUTO_INCREMENT,
     burger_name VARCHAR(255),
     devoured BOOLEAN,
+    PRIMARY KEY(id)
 );
